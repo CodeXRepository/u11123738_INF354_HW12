68a189c2c1b8f775e95df418033978baf5d89626
CodeXRepository <mpho.moraba101@gmail.com>
2015-05-27 15:14:52 +0200

    Update Repository.php

--- Repository.php
+++ Repository.php
@@ -1,34 +1,37 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<?php
-
-<div style = “color:red”>
-<div style = “font:Arial”>
-define("ZBL MABUNDA u11123738");
-echo constant("ZBL MABUNDA u11123738");
-<div>
-
-<div style = “color:brown”>
-<div style = “font:TimesNewRoman”>
-define("R Singo u11059584");
-echo constant("R Singo u11059584");
-<div>
-
-<div style = “color:green”>
-<div style = “font:Segoe UI”>
-define("B Hlophe u11315653");
-echo constant("B Hlophe u11315653");
-<div>
-
-<div style = “color:blue”>
-<div style = “font:Calibri”>
-define("MMC Moraba u29635030");
-echo constant("MMC Moraba u29635030");
-<div>
-
-?>
-
-</body>
-</html>
+	<head>
+		<title>u11123738_INF354_HW12</title>
+		<style>
+			p{color:blue} body{background-color:yellow} 
+			#div1{color:red; font-family: Arial Black; font-size:18px;} #div1:hover{color:blue;font-size:20px;}
+			#div2{color:red; font-family: Calibri; font-size:18px;} #div2:hover{color:purple;font-size:20px;}
+			#div3{color:red; font-family: TimesNewRoman; font-size:18px;} #div3:hover{color:black;font-size:20px;}
+			#div4{color:red; font-family: Segoe UI; font-size:18px;} #div4:hover{color:green;font-size:20px;}
+		</style>
+	</head>
+	<body>
+		<div id="div1">
+			<?php
+				echo "ZBL MABUNDA u11123738";
+			?>
+		</div>
+		<div id="div2">
+			<?php
+				echo "MMC Moraba u29635030";
+			?>
+		</div>
+		<div id="div3">
+		<?php
+				echo "R Singo u11059584";
+			?>
+		
+		</div>
+		<div id="div4">
+		<?php
+				echo "B Hlophe u11315653";
+			?>
+		
+		</div>
+	</body>
+</html> 
